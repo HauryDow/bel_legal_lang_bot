cc30260792859ae3c634540be86b953dda0d8a34
HauryDow <100564592+HauryDow@users.noreply.github.com>
2022-05-18 08:22:02 +0300

    Update Bel_legal_language_bot.py
    changed completely

--- Bel_legal_language_bot.py
+++ Bel_legal_language_bot.py
@@ -1,18 +1,28 @@
 """
-the Bot makes everyday posts with Belarusial legal words and their definition
+the Bot makes everyday posts with some picture+Belarusial legal word and its definition
 """
 
-import telebot
+#!/usr/bin/python3
+
+import telegram
 import time
-import random
-import config1
-bot = telebot.TeleBot(config1.token)
-channel_name = config1.channel_name
-f = open('dictionary.txt', 'r', encoding='UTF-8')
+
+TOKEN = 'your_token_here'
+bot = telegram.Bot(token=TOKEN)
+chat_id='your_chat_id_here'
+f = open('words1.txt', 'r', encoding='UTF-8')
 words = f.read().split('\n')
 f.close()
+temp = ''
+num=1
 for word in words:
-    post=" "+"\n"+"\n"+ "#беларускіюрыст"+"\n"+"\n"+word
-    bot.send_message(channel_name, post)
-    time.sleep(3600)
-
+    temp += word + '\n'
+    if word[-1] == '*':
+        post = "#беларуская_лексіка #родныя_словы" + "\n" + "\n" + temp[:-2:]
+        #pictures named 1.jpg, 2.jpg, 3.jpg, etc.
+        name='folder_with_pictures'+str(num)+'.jpg'
+        with open(name, 'rb') as photo:
+            bot.send_photo(chat_id, photo, post)
+        temp = ''
+        num+=1
+        time.sleep(86400)
